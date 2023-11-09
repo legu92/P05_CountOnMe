c477575a0f1447c2ac330166f39235fc626177df
legu92 <143726053+legu92@users.noreply.github.com>
2023-11-09 22:42:17 +0100

    CalcModel ready with test, beafore migrating the controller

--- CountOnMe/Model/CalcModel.swift
+++ CountOnMe/Model/CalcModel.swift
@@ -8,6 +8,140 @@
 
 import Foundation
 
+enum Operand {case add, subs, mult, div}
+
 class CalcModel {
+    static let aszOperatorsAvailable: [Operand : String] = [.add : "+", .subs : "-", .div : "÷", .mult : "x"]
     
+    private var szExpression: String = ""
+    
+    /// Give back a string with all operators available as an visual operand
+    static var szAllOperators: String {
+        let (_,szOperators) = CalcModel.aszOperatorsAvailable.reduce(("","")) {return ("", $0.1 + $1.1)}
+        return szOperators
+    }
+    
+    private var aszElements: [String] {
+        return self.szExpression.split(separator: " ").map { "\($0)" }
+    }
+    
+    /// To know if an opérator can be add at the ennd
+    var bCanAddOperator: Bool {
+        guard let szLastElement = self.aszElements.last else {
+            return false
+        }
+        return !CalcModel.szAllOperators.contains(szLastElement)
+    }
+    
+    /// To know if the expression can be calculated
+    var bExpressionCanBeCalculated: Bool {
+        if self.aszElements.count == 1 {
+            return false
+        }
+        
+        return self.aszElements.count % 2 == 1
+    }
+    
+    var bExpressionHaveResult: Bool {
+        return self.szExpression.firstIndex(of: "=") != nil
+    }
+    
+    /// Add one digit (0-9) to the expression
+    /// - Parameter szDigit: digit to add
+    /// - Returns: return true if the digit could have been added, else return false
+    func addDigit(digit szDigit: String) -> Bool {
+        if szDigit.count != 1 || !"0123456789".contains(szDigit) {
+            return false
+        }
+        
+        // if the user have a calculuted expression displayed
+        // then the expression is erased
+        if self.bExpressionHaveResult {
+            eraseExpression()
+        }
+        
+        self.szExpression.append(szDigit)
+        
+        return true
+    }
+    
+    func addOperator(with cOperator: Operand) -> Bool {
+        var bReturn: Bool = true
+        
+        if self.bCanAddOperator {
+            // If the precedent expression has been calculated
+            // then the result is conserved for the first operand
+            if self.bExpressionHaveResult {
+                if let szLastElement = self.aszElements.last {
+                    self.szExpression = szLastElement
+                } else {
+                    eraseExpression()
+                }
+            }
+            switch cOperator {
+                case .add:
+                    self.szExpression.append(" + ")
+                    break
+                    
+                case .subs:
+                    self.szExpression.append(" - ")
+                    break
+                    
+                case .mult:
+                    self.szExpression.append(" x ")
+                    break
+                    
+                case .div:
+                    self.szExpression.append(" ÷ ")
+                    break
+            }
+        } else {
+            bReturn = false
+        }
+        
+        return bReturn
+    }
+    
+    func getExpression() -> String {
+        return self.szExpression
+    }
+    
+    func calculateExpression() -> Bool {
+        var bReturn: Bool = false
+        
+        if self.bExpressionCanBeCalculated {
+            // Create local copy of operations
+            var aszOperationsToReduce = self.aszElements
+            
+            // Iterate over operations while an operand still here
+            while aszOperationsToReduce.count > 2 {
+                let iLeft = Int(aszOperationsToReduce[0])!
+                let szOperand = aszOperationsToReduce[1]
+                let iRight = Int(aszOperationsToReduce[2])!
+                
+                let iResult: Int
+                switch szOperand {
+                    case "+": iResult = iLeft + iRight
+                    case "-": iResult = iLeft - iRight
+                    case "x": iResult = iLeft * iRight
+                    case "÷": iResult = iLeft / iRight
+                    default: fatalError("Unknown operator !")
+                }
+                
+                aszOperationsToReduce = Array(aszOperationsToReduce.dropFirst(3))
+                aszOperationsToReduce.insert("\(iResult)", at: 0)
+            }
+            
+            self.szExpression.append(" = \(aszOperationsToReduce.first!)")
+            
+            bReturn = true
+        }
+        
+        return bReturn
+    }
+    
+    func eraseExpression() {
+        self.szExpression = ""
+    }
+
 }
--- CountOnMeTests/CalcModelTest.swift
+++ CountOnMeTests/CalcModelTest.swift
@@ -10,11 +10,308 @@ import XCTest
 @testable import CountOnMe
 
 final class CalcModelTest: XCTestCase {
-
-    func testGiven_WhenAskForCalcModel_ThenCalcModelExists() {
-        let oCalcModel = CalcModel()
+    
+    var oCalcModel: CalcModel! = nil
+    
+    override func setUp() {
+        super.setUp()
+        oCalcModel = CalcModel()
+    }
+    
+    func testGivenCalcModelJustCreated_WhenAskingForExpression_ThenExpressionIsAEmptyString() {
+        // MARK: - GIVEN
         
-        XCTAssertNotNil(oCalcModel)
+        
+        // MARK: - WHEN
+        let szExpresion = oCalcModel.getExpression()
+        
+        // MARK: - THEN
+        XCTAssertNotNil(szExpresion)
+        XCTAssertEqual("",szExpresion)
+        
+    }
+    
+    func testGivenExpressionIsEmpty_WhenAddingADigit_ThenExpressionContainJustTheDigit() {
+        // MARK: - GIVEN
+        
+        // MARK: - WHEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "1")
+    }
+    
+    func testGivenExpressionHave1value_WhenAddingADigit2_ThenExpressionContains12() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        
+        // MARK: - WHEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "2"))
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "12")
+    }
+    
+    func testGivenExpressionHave1value_WhenAddingANonDigit_ThenExpressionContains1() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        
+        // MARK: - WHEN
+        XCTAssertFalse(oCalcModel.addDigit(digit: "h"))
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "1")
+    }
+    
+    func testGivenExpressionHave1value_WhenAdding2Digits_ThenExpressionContains1() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        
+        // MARK: - WHEN
+        XCTAssertFalse(oCalcModel.addDigit(digit: "12"))
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "1")
+    }
+    
+    func testGivenExpressionHave1value_WhenAdding0Digit_ThenExpressionContains1() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        
+        // MARK: - WHEN
+        XCTAssertFalse(oCalcModel.addDigit(digit: ""))
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "1")
+    }
+    
+    func testGivenExpressionHave1Plusvalue_WhenAdding2AsDigit_ThenExpressionContains1Plus2() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .add))
+        
+        // MARK: - WHEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "2"))
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "1 + 2")
+    }
+    
+    func testGivenExpressionHaveNovalue_WhenAddindPlusOperand_ThenOperationIsNotPermettedAndExpressionNotChanged() {
+        // MARK: - GIVEN
+        
+        // MARK: - WHEN
+        XCTAssertFalse(oCalcModel.addOperator(with: .add))
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "")
+    }
+    
+    func testGivenExpressionHas1PlusValue_WhenAddindPlusOperand_ThenOperationIsNotPermettedAndExpressionNotChanged() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .add))
+        
+        // MARK: - WHEN
+        XCTAssertFalse(oCalcModel.addOperator(with: .add))
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "1 + ")
+    }
+    
+    func testGivenExpressionIs1Plus2_WhenAskingResult_ThenExpressionIs1Plus2Equal3() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .add))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "2"))
+        
+        // MARK: - WHEN
+        XCTAssertTrue(oCalcModel.calculateExpression())
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "1 + 2 = 3")
+        
+    }
+    
+    func testGivenExpressionIs2Minus1_WhenAskingResult_ThenExpressionIs2Minus1Equal1() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "2"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .subs))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        
+        // MARK: - WHEN
+        XCTAssertTrue(oCalcModel.calculateExpression())
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "2 - 1 = 1")
+        
+    }
+    
+    func testGivenExpressionIs20Minus10_WhenAskingResult_ThenExpressionIs20Minus10Equal10() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "2"))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "0"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .subs))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "0"))
+        
+        // MARK: - WHEN
+        XCTAssertTrue(oCalcModel.calculateExpression())
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "20 - 10 = 10")
+        
+    }
+    
+    func testGivenExpressionIs2Minus10_WhenAskingResult_ThenExpressionIs2Minus1EqualMinus8() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "2"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .subs))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "0"))
+        XCTAssertEqual(oCalcModel.getExpression(), "2 - 10")
+        
+        // MARK: - WHEN
+        XCTAssertTrue(oCalcModel.calculateExpression())
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "2 - 10 = -8")
+    }
+    
+    func testGivenExpressionIs2Minus_WhenAskingResult_ThenExpressionNotChangeAndIs2Minus() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "2"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .subs))
+        
+        // MARK: - WHEN
+        XCTAssertFalse(oCalcModel.calculateExpression())
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "2 - ")
+        
+    }
+    
+    func testGivenExpressionIs2_WhenAskingResult_ThenExpressionNotChangeAndIs2() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "2"))
+        
+        // MARK: - WHEN
+        XCTAssertFalse(oCalcModel.calculateExpression())
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "2")
+    }
+    
+    func testGivenExpressionIs10Minus1Plus_WhenAskingResult_ThenExpressionNotChangeAndIs10Minus1Plus() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "0"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .subs))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .add))
+
+        // MARK: - WHEN
+        XCTAssertFalse(oCalcModel.calculateExpression())
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "10 - 1 + ")
+    }
+    
+    func testGivenExpressionIs10Minus1Plus2_WhenAskingResult_ThenExpressionIs10Minus1Plus2Equal11() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "0"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .subs))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .add))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "2"))
+
+        // MARK: - WHEN
+        XCTAssertTrue(oCalcModel.calculateExpression())
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "10 - 1 + 2 = 11")
+    }
+    
+    func testGivenExpressionIs10Minus1Plus2_WhenEraseExpression_ThenExpressionIsEmpty() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "0"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .subs))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .add))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "2"))
+
+        // MARK: - WHEN
+        oCalcModel.eraseExpression()
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "")
+    }
+    
+    func testGivenExpressionIs20Mult10_WhenAskingResult_ThenExpressionIs20Mult10Equal200() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "2"))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "0"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .mult))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "0"))
+        
+        // MARK: - WHEN
+        XCTAssertTrue(oCalcModel.calculateExpression())
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "20 x 10 = 200")
+        
+    }
+    
+    func testGivenExpressionIs20Div10_WhenAskingResult_ThenExpressionIs20Div10Equal2() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "2"))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "0"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .div))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "0"))
+        
+        // MARK: - WHEN
+        XCTAssertTrue(oCalcModel.calculateExpression())
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "20 ÷ 10 = 2")
+        
+    }
+    
+    func testGivenExpressionWithResult20Div10Equal2_WhenAddingDigit2_ThenExpressionIs2() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "2"))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "0"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .div))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "0"))
+        XCTAssertTrue(oCalcModel.calculateExpression())
+        
+        // MARK: - WHEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "2"))
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "2")
+        
     }
 
+    func testGivenExpressionWithResult20Div10Equal2_WhenAddingPlus_ThenExpressionIs2Plus() {
+        // MARK: - GIVEN
+        XCTAssertTrue(oCalcModel.addDigit(digit: "2"))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "0"))
+        XCTAssertTrue(oCalcModel.addOperator(with: .div))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "1"))
+        XCTAssertTrue(oCalcModel.addDigit(digit: "0"))
+        XCTAssertTrue(oCalcModel.calculateExpression())
+        
+        // MARK: - WHEN
+        XCTAssertTrue(oCalcModel.addOperator(with: .add))
+        
+        // MARK: - THEN
+        XCTAssertEqual(oCalcModel.getExpression(), "2 + ")
+        
+    }
 }
